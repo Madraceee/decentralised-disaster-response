18b189d71b7aec3aea496d73507b8a83ed98bf7a
madraceee <nitheeshraaja2010207@ssn.edu.in>
2022-09-10 15:22:48 +0530

    Modified PriceConvertor.sol

---
+++ contracts/PriceConvertor.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+//Library 
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConvertor{
+    function getPrice() public view returns(uint256){
+
+        AggregatorV3Interface priceFeed=AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada); // Polygon Mumbai Testnet MATIC TO USD
+        (, int256 price, , ,)=priceFeed.latestRoundData(); //Only price        
+        return uint256(price*1e10);
+        
+    }
+
+    function getConversionRate(uint256 maticAmount) public view returns(uint256){
+        uint256 maticPrice=getPrice();
+        uint256 maticAmountInUSD=(maticPrice*maticAmount) / 1e18;
+        uint256 maticAmountInINR = maticAmountInUSD * 79.66;
+        return maticAmountInINR;
+    }
+}
