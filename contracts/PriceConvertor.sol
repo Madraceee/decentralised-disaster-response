35a2f0ea095e0b98547fcf143a38eadd7c158b17
madraceee <nitheeshraaja2010207@ssn.edu.in>
2022-09-10 16:28:57 +0530

    Changed equation for USD TO INR conversion

--- contracts/PriceConvertor.sol
+++ contracts/PriceConvertor.sol
@@ -8,15 +8,16 @@ library PriceConvertor{
     function getPrice() public view returns(uint256){
 
         AggregatorV3Interface priceFeed=AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada); // Polygon Mumbai Testnet MATIC TO USD
-        (, int256 price, , ,)=priceFeed.latestRoundData(); //Only price        
+        (, int256 price, , ,)=priceFeed.latestRoundData(); //Only price      
         return uint256(price*1e10);
         
     }
 
     function getConversionRate(uint256 maticAmount) public view returns(uint256){
         uint256 maticPrice=getPrice();
-        uint256 maticAmountInUSD=(maticPrice*maticAmount) / 1e18;
-        uint256 maticAmountInINR = maticAmountInUSD * 79.66;
+        uint256 maticAmountInUSD=(maticPrice*maticAmount);
+        uint256 maticAmountInINR= (maticAmountInUSD * 79) / 1e18;
         return maticAmountInINR;
     }
+
 }
