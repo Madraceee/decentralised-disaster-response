fded3dc1f8f4bc5ce8779094bf6e36130346e62a
Hareesh S <hareesh.s.teja@gmail.com>
2022-09-11 19:47:03 +0530

    F

--- frontend/disaster-ui/src/App.js
+++ frontend/disaster-ui/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 // import ReactDOM from "react-dom/client";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-} from "react-router-dom";
+// import {
+//     BrowserRouter as Router,
+//     Routes,
+//     Route,
+//     Navigate,
+// } from "react-router-dom";
 
 import './index.css';
-import PageOne from './pageOne';
+// import PageOne from './pageOne';
 import PageTwo from './pageTwo';
 
 export default function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/" element={<PageTwo />} />
-                <Route path="/2" element={<PageOne />} />
-                <Redirect to="/" />
-            </Switch>
-        </Router>
+        // <Router>
+        //     <Routes>
+        //         <Route path="/" element={<PageTwo />} />
+        //         <Route path="/2" element={<PageOne />} />
+        //         <Navigate to="/" />
+        //     </Routes>
+        // </Router>
+
+        <PageTwo />
     )
 }
