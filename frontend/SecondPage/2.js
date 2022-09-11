8e54cc90beb0c95d91a35f0303e9544499366af5
madraceee <nitheeshraaja2010207@ssn.edu.in>
2022-09-11 06:22:26 +0530

    Added verify funtion

--- frontend/SecondPage/2.js
+++ frontend/SecondPage/2.js
@@ -1,8 +1,9 @@
-import { ethers } from "../ethers.js";
+import { ethers } from "./ethers.js";
 import abi from './utils/EventInformationABI.json' assert { type: "json" };
 
 // Function to connect to metamask and get the account
 
+const contractABI = abi;
 const button2 = document.getElementById('connect');
 const connect = async() =>{
     if( typeof window.ethereum !== 'undefined'){
@@ -33,7 +34,49 @@ button1.addEventListener('click',contractAddressGetter);
 
 // Funtion to create a new event
 
-cosnt 
+const button3 = document.getElementById("createEventSubmit");
+const generalInfo = document.getElementById("generalInfo");
 const createEvent = async()=>{
+    if(generalInfo.value.length != 0){
+        const {ethereum} = window;
+        if(ethereum){
+            const provider = new ethers.providers.Web3Provider(window.ethereum); 
+            await provider.send("eth_requestAccounts", []);
+            const Signer = provider.getSigner();         
+            const EventInformation = new ethers.Contract(contractAddress,contractABI, Signer);
 
+            const eventTxn = await EventInformation.createEvent(generalInfo.value,{ value: ethers.utils.parseEther("0.15"), gasLimit: 3000000 });
+            console.log("Mining:",eventTxn);
+            await eventTxn.wait();
+            console.log("Mined");
+        }
+    }
 }
+
+button3.addEventListener('click',createEvent);
+
+// Function for verifiers to verify
+const button4 = document.getElementById("verificationSubmit");
+const verificationID = document.getElementById("verificationID");
+const verificationStatus = document.getElementById("verification");
+
+const verifyEvent = async() => {
+    if(verificationID.value.length!=0 && verificationStatus.value.length!=0){
+        const {ethereum} = window;
+        if(ethereum){
+            const provider = new ethers.providers.Web3Provider(window.ethereum); 
+            await provider.send("eth_requestAccounts", []);
+            const Signer = provider.getSigner();         
+            const EventInformation = new ethers.Contract(contractAddress,contractABI, Signer);
+
+            const eventTxn = await EventInformation.verifiyEvent(verificationID.value,verificationStatus.value,{ value: ethers.utils.parseEther("0.027"), gasLimit: 3000000 }); // Change verifyEvent spelling after updating contract
+            console.log("Mining:",eventTxn);
+            await eventTxn.wait();
+            console.log("Mined");
+        }
+    }
+    
+}
+
+button4.addEventListener('click',verifyEvent);
+
